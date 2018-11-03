2a9d42d23dc078a7619d6cfbe6cc5a629aa5fbfc
Peter Cowal <pcowal@gmail.com>
2018-11-02 20:46:25 -0700

    fixes broken files

--- SuperKeepaway/Assets/Scripts/PlayerControl.cs
+++ SuperKeepaway/Assets/Scripts/PlayerControl.cs
@@ -8,12 +8,10 @@ public class PlayerControl : MonoBehaviour {
     public bool jump = false;
     public float accelerate = 100f;
     public float airAccelerate = 10f;
-    public float runSpeed = 5f;
+    public float runSpeed = 10f;
     public float jumpSpeed = 10f;
 
-    public Transform groundCheck;
-
-    public bool grounded = false;
+    private bool grounded = false;
     private Rigidbody2D rb;
     private BoxCollider2D col;
 
@@ -25,16 +23,20 @@ public class PlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        grounded = Physics2D.BoxCast(transform.position, col.size, 0, Vector2.down, 0.01f, LayerMask.GetMask("Ground"));
-
-        if (Input.GetButtonDown("Jump") && grounded)
+        if (grounded)
         {
-            jump = true;
+            if (Input.GetButtonDown("Jump"))
+            {
+                jump = true;
+            }
         }
     }
 
     private void FixedUpdate()
     {
+        grounded = Physics2D.BoxCast(transform.position, col.size, 0, Vector2.down, 0.05f, LayerMask.GetMask("Ground"));
+
+
         float h = Input.GetAxis("Horizontal");
 
         if (Mathf.Abs(h) > 0.5f)
@@ -46,10 +48,32 @@ public class PlayerControl : MonoBehaviour {
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
-        if (jump)
+        if (grounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-            jump = false;
+            rb.gravityScale = 3;
+            if (jump)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+                jump = false;
+            }
+        }
+        else
+        {
+            if (rb.velocity.y > 0 )
+            {
+                if (Input.GetButton("Jump"))
+                {
+                    rb.gravityScale = 2;
+                }
+                else
+                {
+                    rb.gravityScale = 10;
+                }
+            }
+            else
+            {
+                rb.gravityScale = 4;
+            }
         }
     }
 }
